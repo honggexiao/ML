09ddbbbaeea9d7d69c66bb899a222b17abaa7df8
honggexiao <honggexiao@sina.com>
2018-05-22 20:43:54 +0800

    [522]modify bayes classifier

--- bayes/bayesclassifier.py
+++ bayes/bayesclassifier.py
@@ -1,10 +1,7 @@
-import numpy as np
-
 class BayesClassifier:
     """this is bayes classifer, given feature vector,
      output is class labels"""
-    def __init__(self, feature_num, labels_list, feature_val_list):
-        self.features_num = feature_num
+    def __init__(self, labels_list, feature_val_list):
         self.labels_list = labels_list
         self.feature_val_list = list(feature_val_list)
 
@@ -24,21 +21,26 @@ class BayesClassifier:
             self.samples_num = len(samples)
             for line in samples:
                 sample = line[:-1].split(',')
-                label = sample[-1]
+                label = int(sample[-1])
                 class_count[label] = class_count.get(label, 0) + 1
                 for j, val in enumerate(sample[0:-1]):
-                    cond_dic[label][j][val] = cond_dic[label][j][val] +1
+                    try:
+                        val_tmp = int(val)
+                    except Exception as e:
+                        val_tmp = val
+                    cond_dic[label][j][val_tmp] = cond_dic[label][j][val_tmp] + 1
         self.class_count = class_count
         self.cond_dic = cond_dic
 
-    def classify(self, sample):
+    def classify(self, sample, k_lambda=0):
         argm_prob = 0
         sample_label = self.labels_list[0]
         for label in self.labels_list:
-            label_prob = self.class_count[label]/float(self.samples_num)
+            label_cnt = self.class_count[label]
+            label_prob = (label_cnt + k_lambda)/float(self.samples_num + len(self.labels_list)*k_lambda)
             cond_prob = 1.0
             for j, val in enumerate(sample):
-                cond_prob *= self.cond_dic[label][j][val]/1.0
+                cond_prob *= (float(self.cond_dic[label][j][val] + k_lambda)/(label_cnt + k_lambda*len(self.feature_val_list[j])))
             prob = label_prob * cond_prob
             if prob > argm_prob:
                 argm_prob = prob
@@ -48,7 +50,9 @@ class BayesClassifier:
 
 if __name__ == '__main__':
     datafile = 'data.txt'
-    classifier = BayesClassifier(2, [1, -1], [[1, 2, 3], ['S', 'M', 'L']])
+    classifier = BayesClassifier([1, -1], [[1, 2, 3], ['S', 'M', 'L']])
     classifier.process_data(datafile)
     sample = [2, 'S']
-    label, prob = classifier.classify(sample)
+    label, prob = classifier.classify(sample, 1)
+    print('label = %d' %label)
+    print('prob= %f' %prob)
